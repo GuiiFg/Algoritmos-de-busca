98c49c5d56da9929b985732bf766ffc12e732c0e
GuiiFg <farias._02f@outlook.com>
2022-08-22 22:16:01 -0300

    Comecando o GameStateControl

---
+++ NumberPuzzle/gameState.py
@@ -0,0 +1,79 @@
+
+class GameSateControl:
+
+    def __init__(self, state = None):
+
+        """
+        Modelo de estado para uso da classe:
+        state = [[1,2,3],
+                [4,5,6],
+                [7,8,0]]
+
+        Modelo para exportação e loading
+        var = [1,2,3,4,5,6,7,8,0]
+        """
+        
+        # 
+        self.__state = self.__LoadState(state)
+        self.__expandStates = None
+
+        if state != None:
+            self.__GenerateExpandStates()
+
+    @property
+    def state(self):
+        return self.__ExportState()
+
+    @state.setter
+    def state(self, valueList):
+        self.__state = self.__LoadState(valueList)
+
+    # carrega um array para dentro do estado da classe
+    def __LoadState(self, stateList : list):
+
+        layer0 = []
+        layer1 = []
+        layer2 = []
+        
+        for value in stateList:
+            if len(layer0) < 3:
+                layer0.append(value)
+            elif len(layer1) < 3:
+                layer1.append(value)
+            elif len(layer2) < 3:
+                layer2.append(value)
+
+        return [layer0, layer1, layer2]
+
+
+    # exporta o estado do game para um array linear
+    def __ExportState(self):
+        stateList = []
+
+        for layer in self.__state:
+            for value in layer:
+                stateList.append(value)
+
+        return stateList
+
+    def PrintState(self):
+
+        print("--------- Print ---------")
+        print("Estado atual:")
+
+        for layer in self.__state:
+            print("\t" + str(layer))
+
+        print("-------------------------")
+
+    def __GenerateExpandStates(self):
+        pass
+
+
+        
+
+
+
+state = GameSateControl([0,8,7,1,2,3,4,5,6])
+
+state.PrintState()
